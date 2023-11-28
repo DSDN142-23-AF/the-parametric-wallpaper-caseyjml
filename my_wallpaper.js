f6b383863e04eea5e8fff257879b988e0ef15b9d
caseyjml <151981877+caseyjml@users.noreply.github.com>
2023-11-28 17:56:28 +1300

    Update my_wallpaper.js
    cheese x & y update 2

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -31,8 +31,8 @@ function my_symbol() { // draw function
   beginShape(); // body of cheese
   vertex(CheeseX-35, CheeseY-50);
   quadraticVertex(CheeseX-10, CheeseY-50, CheeseX, CheeseY-30);
-  vertex(CheeseX, CheeseHeight);
-  vertex(CheeseX-95, CheeseHeight);
+  vertex(CheeseX, CheeseHeight); // CHANGES CHEESE HEIGHT/THICKNESS
+  vertex(CheeseX-95, CheeseHeight); // CHANGES CHEESE HEIGHT/THICKNESS
   vertex(CheeseX-100, CheeseY-20);
   endShape(CLOSE);
 
@@ -42,18 +42,18 @@ function my_symbol() { // draw function
 
   // CHEESE HOLES:
   fill(224, 192, 74); // cheese shading colour (for holes)
-  ellipse(160, 155, 10, 5);
-  ellipse(135, 135, 20, 5);
-  ellipse(135, 160, 20, 10);
-  ellipse(105, 155, 10, 5);
-  ellipse(160, 130, 10, 5);
-  ellipse(190, 155, 17, 15); // righthand side hole
+  ellipse(CheeseX-30, CheeseY-15, 10, 5);
+  ellipse(CheeseX-55, CheeseY-35, 20, 5);
+  ellipse(CheeseX-55, CheeseY-10, 20, 10);
+  ellipse(CheeseX-85, CheeseY-15, 10, 5);
+  ellipse(CheeseX-30, CheeseY-40, 10, 5);
+  ellipse(CheeseX, CheeseY-15, 17, 15); // righthand side hole
 
   // NEGATIVE SPACE (for cheese holes on border of cheese body)
   fill(240, 255, 240) // background colour
-  ellipse(192, 155, 12, 14); // to cover half the righthand side ellipse
+  ellipse(CheeseX+2, CheeseY-15, 12, 14); // to cover half the righthand side ellipse
   noStroke();
-  rect(191, 145, 8.5, 30) // to remove the stroke of half the above ellipse
+  rect(CheeseX+1, CheeseY-25, 8.5, 30) // to remove the stroke of half the above ellipse
 
 
 // RAT:
