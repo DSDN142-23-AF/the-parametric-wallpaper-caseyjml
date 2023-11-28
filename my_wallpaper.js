e50cc71f39ddaf039988c4b43c3e1aa1cb290d8f
caseyjml <151981877+caseyjml@users.noreply.github.com>
2023-11-28 15:56:05 +1300

    Update my_wallpaper.js
    made cheese height & thickness changeable

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,9 @@
 //your parameter variables go here!
-let CheeseHeight = 170;
-
+let CheeseX = 190 // X location of cheese
+let CheeseY = 170 // Y location of cheese
+let CheeseHeight = CheeseY + 0// height/thickness of the cheese
+let RatX = 60 // X location of rat
+let RatY = 80 // Y location of rat
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -14,15 +17,17 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(240, 255, 240); // light honeydew green colour
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+function my_symbol() { 
+  
+// CHEESE
   strokeWeight(2);
-  stroke(163, 141, 11);
-  fill(252, 231, 111);
+  stroke(163, 141, 11); // dark yellow
+  fill(252, 231, 111); // light yellow
 
-  beginShape(); // Cheese
+  beginShape(); // body of cheese
   vertex(155, 120);
   quadraticVertex(180, 120, 190, 140);
   vertex(190, CheeseHeight);
@@ -30,7 +35,14 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   vertex(90, 150);
   endShape(CLOSE);
 
-  line(90, 150, 190, 140); // Line through middle of cheese
+  line(90, 150, 190, 140); // line through middle of cheese
+
+
+// RAT
+  stroke(50, 54, 53); // dark grey
+  fill(211, 219, 217); // light grey 
+
+  ellipse(RatX, RatY, 50, 70); // rat belly
 
 
 }
