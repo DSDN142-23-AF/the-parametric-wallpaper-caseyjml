919778768ebf179a049f394e7aa9d8cfa0ffdbc5
caseyjml <151981877+caseyjml@users.noreply.github.com>
2023-11-28 17:10:06 +1300

    Update my_wallpaper.js
    cheese holes

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,7 @@
 //your parameter variables go here!
 let CheeseX = 190 // X location of cheese
 let CheeseY = 170 // Y location of cheese
-let CheeseHeight = CheeseY + 0// height/thickness of the cheese
+let CheeseHeight = CheeseY + 0 // height/thickness of the cheese
 let RatX = 60 // X location of rat
 let RatY = 80 // Y location of rat
 
@@ -22,10 +22,10 @@ function wallpaper_background() {
 
 function my_symbol() { 
   
-// CHEESE
+// CHEESE:
   strokeWeight(2);
-  stroke(163, 141, 11); // dark yellow
-  fill(252, 231, 111); // light yellow
+  stroke(163, 141, 11); // cheese line colour
+  fill(252, 231, 111); // cheese fill colour
 
   beginShape(); // body of cheese
   vertex(155, 120);
@@ -36,13 +36,31 @@ function my_symbol() {
   endShape(CLOSE);
 
   line(90, 150, 190, 140); // line through middle of cheese
+  
 
 
-// RAT
+  // CHEESE HOLES:
+  fill(224, 192, 74); // cheese shading colour (for holes)
+  ellipse(160, 155, 10, 5);
+  ellipse(135, 135, 20, 5);
+  ellipse(135, 160, 20, 10);
+  ellipse(105, 155, 10, 5);
+  ellipse(160, 130, 10, 5);
+  ellipse(190, 155, 17, 15); // righthand side hole
+
+  // NEGATIVE SPACE (for cheese holes on border of cheese body)
+  fill(240, 255, 240) // background colour
+  ellipse(192, 155, 12, 14); // to cover half the righthand side ellipse
+  noStroke();
+  rect(191, 145, 10, 30) // to remove the stroke of half the above ellipse
+
+
+// RAT:
   stroke(50, 54, 53); // dark grey
   fill(211, 219, 217); // light grey 
 
   ellipse(RatX, RatY, 50, 70); // rat belly
+
 
 
 }
