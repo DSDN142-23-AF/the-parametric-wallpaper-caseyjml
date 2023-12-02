37e1e1386996fdc6186ec77917de3a52281aa82c
caseyjml <151981877+caseyjml@users.noreply.github.com>
2023-12-02 13:49:14 +1300

    final commit (yay)

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -26,7 +26,7 @@ let FY2 = 85 // 180 // Y location of fly 2
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
-  pWallpaper.resolution(NINE_PORTRAIT);
+  pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
